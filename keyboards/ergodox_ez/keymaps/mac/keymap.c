b9c9caa4c0d1e3e2ea643f027087f8232933fae6
muratas <rw.satoshi.murata@gmail.com>
2019-08-05 22:27:35 +0900

    left

--- keyboards/ergodox_ez/keymaps/mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/mac/keymap.c
@@ -17,19 +17,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  | BkSp   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  | BkSp   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |   '  |   \  |   [  |   ]  | Grv/L1 |
+ *   |ESC/L1| Alt  |AltShf|      |      |                                       |  '"  |   \  |   [  |   ]  | Grv/L1 |
  *   `----------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Cmd  |Space |------|       |------|  Tab   |Enter |
+ *                                 | Cmd  |Space |------|       |------|  Tab   | ESC  |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  *
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_TRNS,
-  KC_DELT,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
-  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,
+  KC_EQL,          KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_TRNS,
+  KC_TAB,          KC_Q,    KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
+  KC_LCTRL,        KC_A,    KC_S,          KC_D,    KC_F,    KC_G,
+  KC_LSFT,         KC_Z,    KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
+  LT(SYMB,KC_ESC), KC_LALT, LALT(KC_LSFT), KC_TRNS, KC_TRNS,
                                                            ALT_T(KC_APP), KC_LGUI,
                                                                           KC_HOME,
                                                          KC_LGUI, KC_SPC, KC_END,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_QUOT, KC_BSLS, KC_LBRC, KC_RBRC,           LT(SYMB,KC_GRV),
   KC_LALT, CTL_T(KC_ESC),
   KC_PGUP,
-  KC_PGDN, KC_TAB, KC_ENT
+  KC_PGDN, KC_TAB, KC_ESC
 ),
 /* Keymap 1: Symbol Layer
  *
