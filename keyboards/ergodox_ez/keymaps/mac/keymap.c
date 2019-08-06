d32165fd8b2a961051203a4805b6b9e7620c723b
muratas <rw.satoshi.murata@gmail.com>
2019-08-06 09:01:47 +0900

    整形

--- keyboards/ergodox_ez/keymaps/mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/mac/keymap.c
@@ -42,18 +42,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTRL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
   KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
   LT(SYMB,KC_ESC), KC_LALT, KC_LGUI, KC_LALT, KC_LGUI,
-                                                           KC_PERC, KC_LGUI,
-                                                                    KC_HOME,
-                                                    KC_SPC, KC_ESC, KC_END,
-  // right hand
-  KC_EQL,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,
-  TG(MDIA),     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_BSPC,
-                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           LT(MDIA,KC_RSFT),
-                         KC_QUOT, KC_BSLS, KC_LBRC, KC_RBRC,           KC_TRNS,
-  KC_LALT, KC_DQUO,
-  KC_PGUP,
-  KC_PGDN, KC_TAB, KC_CAPS
+                                                       KC_PERC, KC_LGUI,
+                                                                KC_HOME,
+                                               KC_SPC, KC_ESC,  KC_END,
+
+                   // right hand
+                   KC_EQL,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,
+                   TG(MDIA),     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_BSPC,
+                                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+                   MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           LT(MDIA,KC_RSFT),
+                                          KC_QUOT, KC_BSLS, KC_LBRC, KC_RBRC,           KC_TRNS,
+                   KC_LALT, KC_DQUO,
+                   KC_PGUP,
+                   KC_PGDN, KC_TAB, KC_CAPS
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -86,15 +87,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                RGB_MOD, RESET,
                                                         KC_TRNS,
                                       RGB_VAD, RGB_VAI, KC_TRNS,
-  // right hand
-  KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_SLASH, KC_F10,
-  KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR,  KC_F11,
-           KC_DOWN, KC_4,    KC_5,    KC_6,    KC_MINUS, KC_F12,
-  KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_PLUS,  KC_TRNS,
-                    KC_0,    KC_0,    KC_DOT,  KC_EQL,   KC_TRNS,
-  RGB_TOG, RGB_SLD,
-  KC_TRNS,
-  KC_TRNS, RGB_HUD, RGB_HUI
+
+           // right hand
+           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_SLASH, KC_F10,
+           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR,  KC_F11,
+                    KC_DOWN, KC_4,    KC_5,    KC_6,    KC_MINUS, KC_F12,
+           KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_PLUS,  KC_TRNS,
+                             KC_0,    KC_0,    KC_DOT,  KC_EQL,   KC_TRNS,
+           RGB_TOG, RGB_SLD,
+           KC_TRNS,
+           KC_TRNS, RGB_HUD, RGB_HUI
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -127,15 +129,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS, KC_TRNS,
                                                         KC_TRNS,
                                       KC_BTN1, KC_BTN2, KC_TRNS,
-  // right hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_BRMD, KC_BRMU, KC_UP,    KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_LEFT, KC_RIGHT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS,  KC_TRNS, KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_MUTE, KC_VOLD, KC_VOLU
+
+           // right hand
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+           KC_TRNS, KC_TRNS, KC_BRMD, KC_BRMU, KC_UP,    KC_TRNS, KC_TRNS,
+                    KC_TRNS, KC_TRNS, KC_LEFT, KC_RIGHT, KC_TRNS, KC_TRNS,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS,  KC_TRNS, KC_TRNS,
+                             KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+           KC_TRNS, KC_TRNS,
+           KC_TRNS,
+           KC_MUTE, KC_VOLD, KC_VOLU
 ),
 };
 
