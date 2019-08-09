8a410cbdd763626ff54cd86d6b1f88bca149b801
muratas <rw.satoshi.murata@gmail.com>
2019-08-09 13:38:35 +0900

    Symbol layer

--- keyboards/ergodox_ez/keymaps/muratas/keymap.c
+++ keyboards/ergodox_ez/keymaps/muratas/keymap.c
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,------------------------------------------------.
- * | Version |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |   (  |   )  |  F9   |
+ * | Version |      |      |      |      |      |      |           |      |      |      |      |   (  |   )  |  `~   |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+-------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   {  |   }  |  F10  |
+ * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   {  |   }  |  \|   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   [  |   ]  |  F12  |
+ * |         |   #  |   $  |   (  |   )  |  `~  |------|           |------| Down |   4  |   5  |   [  |   ]  |       |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   +  |  F12  |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+-------'
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox(
   // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+  VRSN,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
   KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
   KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_TRNS, KC_TRNS, KC_TRNS,
 
            // right hand
-           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_LPRN, KC_RPRN, KC_F9,
-           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_LCBR, KC_RCBR, KC_F10,
-                    KC_DOWN, KC_4,    KC_5,    KC_LBRC, KC_RBRC, KC_F11,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_GRV,
+           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_LCBR, KC_RCBR, KC_BSLS,
+                    KC_DOWN, KC_4,    KC_5,    KC_LBRC, KC_RBRC, KC_TRNS,
            KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_PLUS, KC_F12,
                              KC_0,    KC_0,    KC_DOT,  KC_EQL,  KC_TRNS,
            KC_TRNS, KC_TRNS,
